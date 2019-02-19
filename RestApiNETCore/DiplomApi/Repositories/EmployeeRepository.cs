2e1798057a3d493fcd9cc81827df72c584a94acf
Lemoriz <alimuahmed.ramil@gmail.com>
2019-02-19 14:48:26 +0500

    [+] #3 Created test api to get data.

---
+++ RestApiNETCore/DiplomApi/Repositories/EmployeeRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace DiplomApi.Repositories
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
+    using Dapper;
+    using Models;
+    using Interfaces;
+
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly IConfiguration _config;
+
+        public EmployeeRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection("Server=DESKTOP-Q31V8AK;Database=Log;User Id=RamilLog; Password=2384548a;");
+            }
+        }
+
+        public async Task<Employee> GetByID(int id)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string sQuery = "SELECT ID, FirstName, LastName FROM Employee WHERE ID = @ID";
+                conn.Open();
+                IEnumerable<Employee> result = await conn.QueryAsync<Employee>(sQuery, new { ID = id });
+                return result.FirstOrDefault();
+            }
+        }
+
+        public async Task<IEnumerable<Employee>> GetAll()
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string sQuery = "SELECT * FROM Employee";
+                conn.Open();
+                IEnumerable<Employee> result = await conn.QueryAsync<Employee>(sQuery);
+                result.Append(new Employee { FirstName = "SEX" });
+                return result;
+            }
+        }
+    }
+}
--- RestApiNETCore/DiplomApi/Startup.cs
+++ RestApiNETCore/DiplomApi/Startup.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace DiplomApi
+﻿namespace DiplomApi
 {
+    using DiplomApi.Repositories;
+    using DiplomApi.Repositories.Interfaces;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -25,6 +20,19 @@ namespace DiplomApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", p =>
+                {
+                    p.AllowAnyOrigin()
+
+                    .AllowAnyHeader()
+
+                    .AllowAnyMethod();
+                });
+            });
+
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -35,13 +43,9 @@ namespace DiplomApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseCors("AllowAll");
         }
     }
 }
