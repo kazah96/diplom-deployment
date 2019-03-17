f48ca47489b9a69148fa8852388a160d6f05f01c
Lemoriz <alimuahmed.ramil@gmail.com>
2019-03-17 21:19:06 +0500

    [+] #15 Added view documents from database.

--- diplom-react-app/src/Document/GetDocument.js
+++ diplom-react-app/src/Document/GetDocument.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import GetDocumentRequest from '../Requests/GetDocumentRequest'
+import { Options } from '../BootstrapConfig/TableConfig'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table-plus';
 
 class GetDocument extends Component {
 	constructor(props) {
@@ -22,15 +24,30 @@ class GetDocument extends Component {
 		if (!isLoaded) {
 			return <div>Loading...</div>;
 		}
+		console.warn(JSON.stringify(items));
 
 		return (
-			<ul className="list-group" key={ items }>
-				{items.map(item => (
-					<li className="list-group-item" key={ item.documentId }>
-						{JSON.stringify(item)}
-					</li>
-				))}
-			</ul>
+			<div class="container">
+				<BootstrapTable 
+					data={ items }  
+					pagination 
+					deleteRow={ true }  
+					insertRow={ true } 
+					search={ true } 
+					selectRow={ { mode: 'checkbox' } } 
+					options={ Options }
+				>
+					<TableHeaderColumn dataField="documentId" isKey dataSort>ID документа</TableHeaderColumn>
+					<TableHeaderColumn dataField="name"  dataSort>Имя документа</TableHeaderColumn>
+					<TableHeaderColumn dataField="shortDiscription"  dataSort>Краткое описание</TableHeaderColumn>
+					<TableHeaderColumn dataField="creationDate"  dataSort>Дата создания</TableHeaderColumn>
+					<TableHeaderColumn dataField="path"  dataSort>Путь к файлу</TableHeaderColumn>
+					<TableHeaderColumn dataField="status"  dataSort>Статус</TableHeaderColumn>
+					<TableHeaderColumn dataField="size"  dataSort>Размер</TableHeaderColumn>
+					<TableHeaderColumn dataField="editsAndChanges"  dataSort>Редактирование и изменение</TableHeaderColumn>
+					<TableHeaderColumn dataField="documentTypeId"  dataSort>Тип документа</TableHeaderColumn>
+				</BootstrapTable>
+			</div>
 		);
 	}
 }
